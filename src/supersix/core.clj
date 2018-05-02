ad02da6f3a7b378082ac733fe9d0f79188c068af
Ninjacop <tlutze@cannonschool.org>
2018-05-02 00:36:31 -0400

    Added swing panel/button/frame

--- src/supersix/core.clj
+++ src/supersix/core.clj
@@ -1,7 +1,25 @@
+
+; Import Swing + Namespace
 (ns supersix.core
-  (:gen-class))
+	(import javax.swing.JFrame)
+	(import javax.swing.JPanel)
+	(import javax.swing.JButton)
+	(import javax.swing.JOptionPane)
+	(import java.awt.event.ActionListener)
+	(:gen-class))
+
+
+; Making the Window variables 
+(def b1 (JButton. "Click Me!"))
+(def panel (doto (JPanel.)
+             (.add b1)))
+(def frame (doto (JFrame. "supersix - 0.1.0 - APLHA CLIENT")
+             (.setSize 200 200)
+             (.setContentPane panel)
+             (.setVisible true)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+	"documentation goes here"
+	[& args]
+	(.setContentPane frame panel)
+	(.revalidate b1))
